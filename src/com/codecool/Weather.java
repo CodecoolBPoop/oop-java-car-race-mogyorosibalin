e3ca583c5dfeb975dbcb1fc59a9ed5643c4b1a2f
mogyorosibalin <mogyorosibalin@gmail.com>
2018-06-22 13:57:20 +0200

    Weather: class is implemented, tests needed

--- src/com/codecool/Weather.java
+++ src/com/codecool/Weather.java
@@ -1,10 +1,19 @@
 package com.codecool;
 
+import java.util.Random;
+
 public class Weather {
-    static boolean isRainingNow;
+    static boolean isRainingNow = false;
+
+    static Random rand = new Random();
 
     static void setRaining() {
         //TODO: There is 30% chance of raining.
+        if (rand.nextInt(101) <= 30) {
+            isRainingNow = true;
+        } else {
+            isRainingNow = false;
+        }
     }
 
     static boolean isRaining() {
